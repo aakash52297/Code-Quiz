60658611c911721e21c06e0b7c5ce366677582e0
Aakash Sharma <aakash.97work@gmail.com>
2021-03-02 08:58:17 -0500

    Updating JavaScript

--- script.js
+++ script.js
@@ -1,29 +1,25 @@
+//Questions and answers
 var questions = [
     {
-        question: "Q1",
-        answers: ["a1: answer 1", "a2: answer 2", "a3: answer 3", "a4: answer 4"],
-        correctAnswer: "a1: answer 1"
+        question: "What is an array?",
+        answers: ["a1: An ordered list of values", "a2: Bucket of fruits", "a3: A computer", "a4: A new song by 50 cent"],
+        correctAnswer: "a1: An ordered list of values"
     },
     {
-        question: "Q2",
-        answers: ["a1: answer 1", "a2: answer 2", "a3: answer 3", "a4: answer 4"],
-        correctAnswer: "a1: answer 1"
+        question: "What is a string?",
+        answers: ["a1: A type of cheese", "a2: Stores a series of characters", "a3: A vegetable", "a4: A brand new toy"],
+        correctAnswer: "a2: Stores a series of charracters"
     },
     {
-        question: "Q3",
-        answers: ["a1: answer 1", "a2: answer 2", "a3: answer 3", "a4: answer 4"],
-        correctAnswer: "a1: answer 1"
+        question: "What is a function?",
+        answers: ["a1: A math problem", "a2: A type of food", "a3: Set of statements that perfoms a task", "a4: A car company"],
+        correctAnswer: "a3: Set of statements that perfoms a task"
     },
     {
-        question: "Q4",
-        answers: ["a1: answer 1", "a2: answer 2", "a3: answer 3", "a4: answer 4"],
-        correctAnswer: "a1: answer 1"
+        question: "What is a variable?",
+        answers: ["a1: A video game", "a2: A candy bar", "a3: An imaginary word", "a4: Anything that can vary"],
+        correctAnswer: "a4: Anything that can vary"
     },
-    {
-        question: "Q5",
-        answers: ["a1: answer 1", "a2: answer 2", "a3: answer 3", "a4: answer 4"],
-        correctAnswer: "a1: answer 1"
-    }
 ]
 var timerId; 
 //variables to hold dom elements
@@ -37,7 +33,7 @@ var feedbackEl = document.getElementById("feedback");
 var startPageEl = document.getElementById("start-page");
 
 function beginQuiz(){
-    // hide start page
+    //hide start page
     startPageEl.setAttribute("class"); 
 }
 
